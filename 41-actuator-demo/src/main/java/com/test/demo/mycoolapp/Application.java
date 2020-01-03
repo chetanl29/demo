db09424cc4606bd5a222fc413fe810a46a3320fb
chetanl29 <chetan011993@gmail.com>
2020-01-03 15:35:11 +0530

    committed on 03-01-2020 15:35

---
+++ 41-actuator-demo/src/main/java/com/test/demo/mycoolapp/Application.java
@@ -0,0 +1,28 @@
+package com.test.demo.mycoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	/*
+	 * List of Spring Actuator endpoints
+	 * 
+	 * 1. health
+	 * 2. info
+	 * 3. beans
+	 * 4. threaddump
+	 * 5. mappings
+	 * 
+	 * 
+	 * */
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		/*1. SpringApplication class starts an Embedded server Tomcat etc..., 
+		 *2. Creates ApplicationContext and registers all beans
+		 *3. Bootstraps your Spring Boot Application
+		 *4. Always place your main Application class in root package above your other packages*/
+	}
+
+}
