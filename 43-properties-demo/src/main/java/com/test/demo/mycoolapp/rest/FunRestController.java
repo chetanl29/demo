4d63794c5de09b34abcedf1ca507be962cd71e1b
chetanl <chetan.l@antaressystems.com>
2020-01-08 18:30:32 +0530

    committed on 08-01-2020

---
+++ 43-properties-demo/src/main/java/com/test/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,43 @@
+package com.test.demo.mycoolapp.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+	
+	//inject properties for coach.name and team.name
+	
+	@Value("${coach.name}")
+	private String coachName;
+	
+	@Value("${team.name}")
+	private String teamName;
+	
+	//new endpoint for teamInfo
+	@GetMapping("/teaminfo")
+	public String teamInfo() {
+		return "Coach Name: "+coachName+" Team Name: "+teamName;
+	}
+	
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello World! Time on server is "+LocalDateTime.now();
+	}
+	
+	//expose a new endpoint for "workout" after dev-tools are added
+	@GetMapping("/workout")
+	public String getDailyWorkOut() {
+		return "Run a Hard 5K";
+	}
+	
+	//expose a new endpoint for "fortune" after dev-tools are added
+	@GetMapping("/fortune")
+	public String getDailyFotune() {
+		return "Today is your lucky day.";
+	}
+
+}
